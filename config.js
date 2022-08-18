62abab88bd8431e5c43e275254aeff4b6d67ff45
Nguyen Tran Bao Duy <duynguyenn1711@gmail.com>
2022-08-19 02:44:07 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = '';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Heyyy Click vào hộp nàyyy bé gì ơi';
+const titleCard = 'Xin chào bà dà :)))';
+const contentCard = 'Ùm thì lần đầu tui làm mấy trò này nên tui làm còn hơi xấu với văn vẻ hơi lủng củng bà thông cảm nha';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'anh2.jpg';
+const base64 = '';
+// const giftImageBase64 = "data:image/png;base64, " + base64;
